5c842bffe8090bfc9ae9dcb0b4d505b47d587ecb
Vikash Sharma <vikash.sharma@zeuslearning.com>
2013-03-23 11:25:00 +0530

    Modify show post ui
    - Modify show post ui.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :status, :uid
   def full_name
-    self.first_name << " "<< self.last_name
+    "#{first_name} #{last_name}".strip
   end
 end
