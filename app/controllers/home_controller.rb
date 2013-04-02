1e856fb5f5abf691a7fde4edaac3a0bd2030403c
Vikash Sharma <vikash.sharma@zeuslearning.com>
2013-04-02 11:30:28 +0530

    Add sign_in link to home page
    - Add sign_in link to home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   #before_filter :login_required#, :except => :index
 
   def index
+    return nil unless session[:user_id]
+    @current_user ||= User.find_by_uid(session[:user_id]['uid'])
   end
 end
