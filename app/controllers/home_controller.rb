6eca21ac29da3b81468ef7fca4c756ff7f93aa7e
Vikash Sharma <vikash.sharma@zeuslearning.com>
2013-04-09 16:51:09 +0530

    Sign in when user is logged in at provider
    - Sign in when user is logged in at provider.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,17 @@ class HomeController < ApplicationController
   def index
     if session[:user_id]
       redirect_to '/show_post'
+    else
+      if cookies[:current_user_single_sign_on_id]
+        respond_to do |format  |
+            format.html  {
+              redirect_to '/auth/concordid'
+            }
+            format.json {
+              render :json => { 'error' => 'Access Denied' }.to_json
+            }
+        end
+      end
     end
   end
 end
